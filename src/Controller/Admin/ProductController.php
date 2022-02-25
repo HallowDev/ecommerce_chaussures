b28e5328a0fa861dba6925bb43d010995b564c4f
HallowDev <91539848+HallowDev@users.noreply.github.com>
2022-02-25 11:26:45 +0100

    Update ProductController.php

--- src/Controller/Admin/ProductController.php
+++ src/Controller/Admin/ProductController.php
@@ -1,23 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-namespace App\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-
-class ProductController extends AbstractController
-{
-    #[Route('/product', name: 'product')]
-    public function index(): Response
-    {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
-        ]);
-    }
-}
-=======
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
