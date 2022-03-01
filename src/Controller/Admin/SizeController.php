760d2c3fa296a51854c80e86f7dbf3af7218a8c2
Saliha Ichou <oum.hanane@gmail.com>
2022-03-01 14:45:06 +0100

    crud size

--- src/Controller/Admin/SizeController.php
+++ src/Controller/Admin/SizeController.php
@@ -2,17 +2,80 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Size;
+use App\Form\SizeType;
+use App\Repository\SizeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('admin/size')]
 class SizeController extends AbstractController
 {
-    #[Route('admin/size', name: 'size')]
-    public function index(): Response
+    #[Route('/', name: 'admin_size_index', methods: ['GET'])]
+    public function index(SizeRepository $sizeRepository): Response
     {
         return $this->render('admin/size/index.html.twig', [
-            'controller_name' => 'SizeController',
+            'sizes' => $sizeRepository->findAll(),
         ]);
+    }
+
+    #[Route('admin/new', name: 'admin_size_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $size = new Size();
+        $form = $this->createForm(SizeType::class, $size);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($size);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_size_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin/size/new.html.twig', [
+            'size' => $size,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'admin_size_show', methods: ['GET'])]
+    public function show(Size $size): Response
+    {
+        return $this->render('admin/size/show.html.twig', [
+            'size' => $size,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'admin_size_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Size $size, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(SizeType::class, $size);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_size_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin/size/edit.html.twig', [
+            'size' => $size,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'admin_size_delete', methods: ['POST'])]
+    public function delete(Request $request, Size $size, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$size->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($size);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_size_index', [], Response::HTTP_SEE_OTHER);
     }
 }
